9257f4d920a18d3b3246036f6a9319f4c712e778
elhalj <wilsonikedakoffi7@gmail.com>
2025-01-10 21:51:57 +0000

    void

--- backend/models/articleModel.js
+++ backend/models/articleModel.js
@@ -1,18 +1,17 @@
-
 import mongoose from "mongoose";
 
 const articleShema = new mongoose.Schema({
-    name:{type:String, require:true},
-    image:{type:String, require:true},
-    price:{type:String, require:true},
-    category:{type:String, require:true},
-    description:{type:String, require:true},
-    stock:{type:Number, require:true},
-    date:{
-        type:Date,
-        default:Date.now(),
-    },
-})
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: String, required: true },
+  category: { type: String, required: true },
+  description: { type: String, required: true },
+  stock: { type: Number, require: true },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
 const ArticleModel = new mongoose.model("Article", articleShema);
 export default ArticleModel;
