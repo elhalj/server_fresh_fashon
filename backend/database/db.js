7e3be581ced79719febeeabebb30f74cf1353174
elhalj <wilsonikedakoffi7@gmail.com>
2025-01-13 22:26:43 +0000

    void

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,20 +4,10 @@ import mongoose from "mongoose";
 
 
 
-// export async function connectDB() {
-//     try {
-//         await mongoose.set("strictQuery",false);
-//         // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-//         await mongoose.connect("mongodb://localhost:27017/produit");
-//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//     } catch (error) {
-//         console.log('erreur de connection ', error)
-//     }
-// }
+
 
 export const connectDB = async () => {
     try {
-        console.log('uri mongo: ', process.env.MONGO_URI)
         const db = await mongoose.connect(process.env.MONGO_URL)
         console.log(`MongoDb connected: ${db.connection.host}`)
     } catch (error) {
@@ -26,21 +16,3 @@ export const connectDB = async () => {
     }
 }
 
-
-// class Database {
-//     constructor(){
-//         this._connect()
-//     }
-
-//     _connect(){
-//         mongoose.connect(process.env.MONGO_URI)
-//             .then(() => {
-//                 console.log("conneter a la base de donnee avec succes");
-//             })
-//             .catch((error) => {
-//                 console.log(error.message);
-//             })
-//     }
-// }
-
-// export default Database;
